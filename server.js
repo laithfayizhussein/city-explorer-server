947ed0b2b22664324791ff0c8974fa44fb7df68d
laithfayizhussein <laithalsanory9919@gmail.com>
2021-06-14 21:28:57 +0300

    07

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
  
-
+const data = require('./wether.json')
 const cors = require('cors');
 
 app.use(cors()) // after you initialize your express app instance 
 
 require('dotenv').config();
-const port =process.env.port ;
+const port =process.env.PORT ;
 // a server endpoint 
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
+  res.json(data) // our endpoint function response
 })
  
 app.listen(port) // kick start the express server to work
