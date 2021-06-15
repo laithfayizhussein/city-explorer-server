50312e7b94f1fd12084b2d996303e72a11e29c2f
laithfayizhussein <laithalsanory9919@gmail.com>
2021-06-15 18:11:30 +0300

    lap8

--- server.js
+++ server.js
@@ -1,17 +1,51 @@
-const express = require('express') // require the express package
-const app = express() // initialize your express app instance
- 
-const data = require('./wether.json')
+
+
+const express = require('express') 
+const app = express()
+const weatherData = require('./wether.json');
+const axios = require('axios');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
 const cors = require('cors');
 
-app.use(cors()) // after you initialize your express app instance 
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
 
-require('dotenv').config();
-const port =process.env.PORT ;
-// a server endpoint 
-app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.json(data) // our endpoint function response
-})
- 
-app.listen(port) // kick start the express server to work
+app.get('/weather', (req, res) => {
+
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    // console.log(lat);
+    // console.log(lon);
+    if (lat && lon) {
+        const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
+
+        axios.get(weatherBitUrl).then(response => {
+            const responseData = response.data.data.map(obj => new Weather(obj));
+            res.json(responseData)
+        }).catch(error => {
+            res.send(error.message)
+        });
+    } else {
+        res.send('please check the lat & lon')
+    }
+
+
+});
+
+
+class Weather {
+    constructor(weatherData) {
+        this.description = weatherData.weather.description;
+        this.date = weatherData.valid_date;
+
+    }
+}
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+});
